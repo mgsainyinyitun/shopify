b9f0f523bd530092b7a67e3eefd28f96b20dddd7
Sai Nyi Nyi Tun <mgsainyinyitun.tumdy@gmail.com>
2024-04-16 15:12:56 +0630

    change icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,14 @@
 import { Box } from "@mui/material";
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import LanguageIcon from '@mui/icons-material/Language';
+import icon from "../../imgs/icon/iconpng.jpg";
 
 function Header(){
     return (
         <header>
-            <Box  p={1} sx={{borderBottom:"1px solid gray", zIndex:1, background:'white', display:'flex',justifyContent:'space-between',position:'fixed',top:0,width:'100%'}}>
+            <Box  p={1} sx={{borderBottom:"1px solid rgba(255,0,0,0.1)", zIndex:1, background:'white', display:'flex',justifyContent:'space-between',position:'fixed',top:0,width:'100%'}}>
                 <Box ml={1} sx={{display:'flex',alignItems:'center'}}>
                     <Box>
-                        <ShoppingCartIcon/>
-                    </Box>
-                    <Box>
-                        Shopify
+                       <img src={icon} width={120}/>
                     </Box>
                 </Box>
                 <Box mr={1}>
