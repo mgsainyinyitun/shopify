e6c64dd1a0ad922b79f957b3af70dc5d8e3e0eac
Sai Nyi Nyi Tun <mgsainyinyitun.tumdy@gmail.com>
2024-05-15 23:26:39 +0630

    refactor constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,17 +1,9 @@
-import { Contrast, ProductionQuantityLimits } from "@mui/icons-material";
-import { startTransition } from "react";
 
-
-export const API_HOST = "http://localhost:8080/api";
-
-
+// export const API_HOST = "http://localhost:8080/api";
 // export const API_HOST = process.env.VITE_API_HOST?process.env.VITE_API_HOST:"https://shopify-api-4nvg.onrender.com/api";
-
 // console.log("API_HOST is:", API_HOST);
 
-// export const API_HOST = "https://shopify-api-4nvg.onrender.com/api";
-
-// export const API_HOST = "http://47.128.145.68:8080/api"
+export const API_HOST = "https://shopify-api-4nvg.onrender.com/api";
 
 
 // 1 - Itaú Unibanco  
